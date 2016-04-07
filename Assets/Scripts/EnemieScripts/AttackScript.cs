20ba905fadc6c6c459d3fbe8a34a0053e2f701de
Florent haxha <kryptag@gmail.com>
2016-04-07 10:09:32 +0200

    EnemyAttack Version0.1

---
+++ Assets/Scripts/EnemieScripts/AttackScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackScript : MonoBehaviour {
+	public double attackDamage;
+	public int attackSpeed;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		GameObject thePlayer = GameObject.Find ("Player");
+		HealthScript HPScript = thePlayer.GetComponent<HealthScript>();
+
+		if (Time.fixedTime % attackSpeed == 0) {
+			Debug.Log (HPScript.healthPoints - 10D);
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		
+
+
+	}
+}
---
+++ Assets/Scripts/PlayerScripts/HealthScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthScript : MonoBehaviour {
+	public double healthPoints;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
